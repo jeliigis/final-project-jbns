7c03529b19e90b85fd6681758baf191e53d3abe9
jeliigis <jelenabarbara.naef@uzh.ch>
2025-10-28 10:47:56 +0100

    first initial test edit to our app

--- app/app_health.py
+++ app/app_health.py
@@ -1,2 +1,4 @@
 import streamlit as st
 st.title('Final Project Jelena and Jeremy')
+st.header("Welcome to our Dashboard!")
+st.markdown("With our Data Analysis we want to sho where bottlenecks occur, where there is potential overcapacities and how hospitals and planners can work with that.")
