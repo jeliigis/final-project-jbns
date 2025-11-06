97008c82d7e61d99102f5a428f8692ec1f568512
jeliigis <jelenabarbara.naef@uzh.ch>
2025-11-06 14:08:45 +0100

    streamlit test

--- app/app_health.py
+++ app/app_health.py
@@ -1,4 +1,4 @@
 import streamlit as st
-st.title('Final Project Jelena and Jeremy')
+st.title('Final Project Team igis')
 st.header("Welcome to our Dashboard!")
-st.markdown("With our Data Analysis we want to sho where bottlenecks occur, where there is potential overcapacities and how hospitals and planners can work with that.")
+st.markdown("With our Data Analysis we want to show where bottlenecks occur, where there is potential overcapacities and how hospitals and planners can work with that.")
