f97fea40010dd38e2f8d52237d92fd523087a05b
jeliigis <jelenabarbara.naef@uzh.ch>
2025-11-06 14:27:57 +0100

    dashboard setup test

--- app/app_health.py
+++ app/app_health.py
@@ -1,4 +1,8 @@
+import pandas as pd
 import streamlit as st
 st.title('Final Project Team igis')
 st.header("Welcome to our Dashboard!")
 st.markdown("With our Data Analysis we want to show where bottlenecks occur, where there is potential overcapacities and how hospitals and planners can work with that.")
+
+
+df_health = pd.read_excel("data/df_health.xlsx")
