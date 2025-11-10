62fd0631d79718b7964f834710db7513a75dd757
jeliigis <jelenabarbara.naef@uzh.ch>
2025-11-10 14:57:22 +0100

    some changes on our homepage

--- app/app_health.py
+++ app/app_health.py
@@ -8,7 +8,7 @@ data_page = st.Page("pages/data_overview.py")
 about_us = st.Page("pages/about_us.py")
 about_data = st.Page("pages/about_data.py")
 
-user_pages = [homepage, about_us, data_page, about_data]
+user_pages = [homepage, data_page, about_data, about_us]
 
 pg = st.navigation(user_pages, position="sidebar", expanded=True)
 pg.run()
