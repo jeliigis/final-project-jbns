c0a34eb1f2a653e07325787e1699263988f7de0a
jeliigis <jelenabarbara.naef@uzh.ch>
2025-11-21 20:45:39 +0100

    did some changes on the app presentation, tabs descriptions

--- app/app_health.py
+++ app/app_health.py
@@ -3,12 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-homepage = st.Page("pages/homepage.py")
-data_page = st.Page("pages/data_overview.py")
-about_us = st.Page("pages/about_us.py")
-about_data = st.Page("pages/about_data.py")
-
-user_pages = [homepage, data_page, about_data, about_us]
+Home = st.Page("pages/Home.py")
+Swiss_Hospital_Data = st.Page("pages/Swiss_Hospital_Data.py")
+Dashboard = st.Page("pages/Dashboard.py")
+user_pages = [Home, Swiss_Hospital_Data, Dashboard]
 
 pg = st.navigation(user_pages, position="sidebar", expanded=True)
 pg.run()
--- app/pages/about_data.py
+++ app/pages/about_data.py
@@ -4,25 +4,30 @@ import streamlit as st
 from pathlib import Path
 
 st.title("About the Data")
-st.markdown("For our project, we used available datasets from the Federal Statistic Office Switzerland.")
+st.markdown(
+    "For our project, we used available datasets from the Federal Statistic Office Switzerland.")
 
-HERE = Path(__file__).resolve().parent #path to app/pages
-DATA_PATH = HERE.parent / "data" / "df_health.xlsx"  #path to app/data/df_health
+HERE = Path(__file__).resolve().parent  # path to app/pages
+DATA_PATH = HERE.parent / "data" / "df_health.xlsx"  # path to app/data/df_health
 df_health = pd.read_excel(DATA_PATH, na_values=["x"])
 
-##displaying the data
-#using placeholders because we want the table to be above the filters
+# displaying the data
+# using placeholders because we want the table to be above the filters
 title_placeholder = st.empty()
 table_placeholder = st.empty()
 
-#adding a filter for years and regions
+# adding a filter for years and regions
 st.subheader("Filter the data")
-years = st.multiselect("Select years", sorted(df_health["Year"].unique()), default=sorted(df_health["Year"].unique()))
-regions = st.multiselect("Select regions", sorted(df_health["Region"].unique()), default=sorted(df_health["Region"].unique()))
+years = st.multiselect("Select years", sorted(
+    df_health["Year"].unique()), default=sorted(df_health["Year"].unique()))
+regions = st.multiselect("Select regions", sorted(
+    df_health["Region"].unique()), default=sorted(df_health["Region"].unique()))
 
-#adding a filter, which columns should be displayed
-categories = st.multiselect("Select columns", df_health.columns, default=df_health.columns)
-df_health = df_health[df_health["Year"].isin(years) & df_health["Region"].isin(regions)][categories]
+# adding a filter, which columns should be displayed
+categories = st.multiselect(
+    "Select columns", df_health.columns, default=df_health.columns)
+df_health = df_health[df_health["Year"].isin(
+    years) & df_health["Region"].isin(regions)][categories]
 
 
 title_placeholder.header("Swiss Hospital Data over the last decade")
