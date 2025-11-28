37e20488a0da37d7c144e0bcb80d800d562d4875
jeliigis <jelenabarbara.naef@uzh.ch>
2025-11-28 10:18:58 +0100

    some cleaning of the home tab

--- app/pages/About_Us.py
+++ app/pages/About_Us.py
@@ -5,9 +5,8 @@ from PIL import Image
 
 
 st.subheader("About us")
-st.markdown("We are Jeremy and Jelena, economics students at UZH. As a part of a module in our bachelors degree program, we examined "
-            "health data from Swiss hospitals over the last decade. We are both completely newbies in programming and data analysis but this project is just "
-            "the start of some further projects. Hopefully : )")
+st.markdown("We are Jeremy and Jelena, economic students at UZH. As a part of a module in our bachelors degree program, we examined "
+            "health data from Swiss hospitals over the last decade.")
 
 if st.button("Jeremy's GitHub"):
     st.markdown("[Open GitHub](https://github.com/jeriigis)")
